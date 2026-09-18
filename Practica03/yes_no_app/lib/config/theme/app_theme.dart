8847e447f14e5aa2af79510e90f16ab257390dae
Jose Francisco Flores Amador <230417@utxicotepec.edu.mx>
2026-09-18 16:54:08 -0600

    Practica03
    Se creó una aplicación Flutter de chat con Material 3, tema personalizado, pantalla de conversación, avatar del contacto y widget reutilizable para mostrar mensajes.

---
+++ Practica03/yes_no_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF5C11D4);
+
+const List<Color> colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert(selectedColor >= 0 && selectedColor < colorThemes.length, 'selectedColor must be between 0 and ${colorThemes.length - 1}');
+
+
+
+
+  ThemeData theme() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colorThemes[selectedColor], 
+      //brightness: Brightness.dark,
+    );
+  }
+}
---
+++ Practica03/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_dubble.dart';
+
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://images.genius.com/63660b8598fe1b71a40aa24b6cd58592.1000x1000x1.jpg',
+            ),
+          ),
+        ),
+        title: const Text('Luis Humberto Navejas'),
+      ),
+      body: const _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  
+                  return const MyMessageDubble();
+                },
+              ), // <-- Aquí cierra el ListView.builder de forma correcta
+            ), // <-- Aquí cierra el Expanded
+
+            const Text('Mundo'),
+          ],
+        ),
+      ),
+    );
+  }
+}
